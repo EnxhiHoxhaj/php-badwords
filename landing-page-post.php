6b2746066380e17caf0894dc1cfcba9e51a91fd0
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-07-24 10:01:43 +0200

    feat: correction

--- landing-page-post.php
+++ landing-page-post.php
@@ -1,13 +1,16 @@
 <?php 
+
+    // paragrafo di testo
     $testo= $_POST['testo'];
+    // parola da censurare
     $badwords= $_POST['badwords'];
-    
+    // lunghezza del paragrazo inserito
+    $paragrafo_len= strlen($testo);
+    // paragrafo censurato
     $testo_corretto= str_replace( $badwords, '***', $testo);
-
+    // lunghezza del paragrafo censurato 
+    $len_corretto= strlen($testo_corretto);
 ?>
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -20,11 +23,18 @@
 <div class="container-fluid">
     <h1 class= "my-3">Form Badwords</h1>
     <div class="row">
+        <!-- paragrafo di testo -->
         <p>Il tuo testo è: <br> <strong><?php echo $testo?></strong></p>
+        <!-- parola da censurare -->
         <p>la parola che vuoi censurare è: <br> <strong><?php echo $badwords?></strong> </p>
+        <!-- lunghezza del paragrazo inserito -->
+        <p>il tuo testo ha questa lunghezza: <br> <strong><?php echo $paragrafo_len?></strong></p>
     </div>
     <div class="row">
+        <!-- paragrafo censurato -->
         <p>Il tuo testo censurato è : <br> <strong><?php echo $testo_corretto?></strong></p>
+        <!-- lunghezza del paragrafo censurato -->
+        <p>il tuo testo ha questa lunghezza: <br> <strong><?php echo $len_corretto?></strong></p>
     </div>
 </div>
 
